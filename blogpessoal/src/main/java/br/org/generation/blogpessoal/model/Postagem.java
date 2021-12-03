b8df35669a00fa7ea227f07d063e990501e19aee
Lara Daniel <laradanielt@gmail.com>
2021-12-03 12:10:58 -0300

    criação classe e métodos Tema

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -1,17 +1,21 @@
 package br.org.generation.blogpessoal.model;
 
+
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_postagens")
@@ -31,6 +35,10 @@ public class Postagem {
 	
 	@UpdateTimestamp
 	private LocalDate data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public Long getId() {
 		return id;
@@ -64,6 +72,13 @@ public class Postagem {
 		this.data = data;
 	}
 	
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 	
 	
 }
